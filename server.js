c4ac4b6adac9c9a013c5d5280a253ea13142080c
BuzzAdonis <55252048+BuzzAdonis@users.noreply.github.com>
2022-01-17 12:32:32 -0400

    hhhh
    hhhh

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/portafolio'));
+app.use(express.static('./dist/portafolioNoob'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/portafolio/'}),
+    res.sendFile('index.html', {root: 'dist/portafolioNoob/'}),
 );
 
 // Start the app by listening on the default Heroku port
